766ef6a8a1072b24bd0a14e1cc28307b2d454cc6
Neal Fultz <nfultz@gmail.com>
2019-05-07 06:19:59 +0000

    incremental update

--- mysession.py
+++ mysession.py
@@ -1,31 +1,38 @@
 # coding: utf-8
+import boto3
+import bs4
 import datetime
 import email
 import email.policy
-with open("Google_Alert_-_Daily_Digest_1.eml") as f:
+import gzip
+import urllib
+
+with open("Google_Alert_-_Daily_Digest_3.eml") as f:
     p = email.message_from_file(f, policy=email.policy.SMTPUTF8)
     
 e = p.get_body("html")
 html = e.get_payload(decode=True)
 with open('em.html', 'wb') as f: f.write(html )
 
-import bs4
-import urllib
 soup = bs4.BeautifulSoup(html, 'html.parser')
 
 
 items = [(tr.div.a, tr.div.div.div) for tr in soup.find_all("tr") if tr.get('itemtype') == "http://schema.org/Article" and tr.div.a ]
 
 
-for i in items:
-    i[0].attrs = {"href":  urllib.parse.parse_qs(urllib.parse.urlparse(i[0]['href']).query)['url'][0] }
-    i[0].span.unwrap()
-    i[1].div.a.span.unwrap()
-    i[1].div.a.unwrap()
-    i[1].div.name = 'em'
-    for t in i[1].descendants: t.attrs = {}
-    i[1].insert(0, i[0])
-    i[1].attrs = {}
+for link, desc in items:
+    link.attrs = {"href":  urllib.parse.parse_qs(urllib.parse.urlparse(link['href']).query)['url'][0] }
+    link.span.unwrap()
+    if link.contents[0] == ' ': 
+        del link.contents[0]
+    if link.contents[-1] == ' ': 
+        del link.contents[-1]
+    desc.div.a.span.unwrap()
+    desc.div.a.unwrap()
+    desc.div.name = 'em'
+    for t in desc.descendants: t.attrs = {}
+    desc.insert(0, link)
+    desc.attrs = {}
 
 clean = bs4.BeautifulSoup("""
 <!doctype html>
@@ -46,24 +53,35 @@ body > div {
 </head>
 <body>
 <h1>neal.news</h1>
-<h3></h3>
 </body>
 </html>""")
-
-for i in items:
-    clean.append(i[1])
 
 # Inline date
 d = p.get("Date")
 d = datetime.datetime.strptime(d, "%a, %d %b %Y %H:%M:%S %z").strftime('%b %d, %Y')
 
 clean.head.title.string = "neal.news / " + d
-clean.body.h3.string = d
 
-with open("index.html", 'w') as f: f.write(clean.prettify())
+h3 = soup.new_tag("h3")
 
-import boto3
+h3.string = d
+
+clean.body.h1.insert_after(h3)
+last = clean.body.h3
+for _, div in items:
+    last.insert_after(div)
+    last = last.next_sibling
+
+
+
+with open("index.html", 'w') as f: f.write(str(clean))
+
 
 # Method 2: Client.put_object()
 client = boto3.client('s3')
-client.put_object(Body=clean.prettify(), Bucket='www.neal.news', Key='index.html', ContentType='text/html')
+client.put_object(
+        Body=gzip.compress(str(clean).encode('UTF-8')),
+        Bucket='www.neal.news',
+        Key='index.html',
+        ContentType='text/html',
+        ContentEncoding='gzip' )
