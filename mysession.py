eae82e5025e204e45a8b7b0ac40559b2c2594893
Neal Fultz <nfultz@gmail.com>
2019-05-21 21:17:52 +0000

    more refactoring

--- mysession.py
+++ mysession.py
@@ -2,113 +2,140 @@
 import boto3
 import bs4
 import datetime
-import email
-import email.policy
+import email, email.policy
 import gzip
 import uuid
 import urllib
 
-with open("Google_Alert_-_Daily_Digest_4.eml") as f:
+def parse_email(f):
     p = email.message_from_file(f, policy=email.policy.SMTPUTF8)
 
-frm, subj = p.get("From"), p.get("Subject")
-    
-e = p.get_body("html")
-html = e.get_payload(decode=True)
-with open('em.html', 'wb') as f: f.write(html )
+    frm, subj = p.get("From"), p.get("Subject")
+        
+    e = p.get_body("html")
+    html = e.get_payload(decode=True)
+    with open('em.html', 'wb') as f: f.write(html )
 
-soup = bs4.BeautifulSoup(html, 'html.parser')
+    # Inline date
+    d = p.get("Date")
+    d = datetime.datetime.strptime(d, "%a, %d %b %Y %H:%M:%S %z")
+    d = d.strftime('%b %d, %Y')
 
+    return bs4.BeautifulSoup(html, 'html.parser'), d
 
-items = [(tr.div.a, tr.div.div.div) for tr in soup.find_all("tr", itemtype="http://schema.org/Article") if tr.div.a ]
-
-
-def unwrap_link(href):
-    url = urllib.parse.urlparse(link['href'])
+def unwrap_link(link):
+    url = urllib.parse.urlparse(link)
     queries = urllib.parse.parse_qs(url.query)
     return {"href":  queries['url'][0] }
 
 
-for i, item in enumerate(items):
-    link, desc = item
-    link.attrs = unwrap_link(link['href'])
-    link.span.unwrap()
-    if link.contents[0]  == ' ':
-        del link.contents[0]
-    if link.contents[-1] == ' ':
-        del link.contents[-1]
+def extract_items(soup):
 
-    #change publisher div to em
-    desc.div.a.span.unwrap()
-    desc.div.a.unwrap()
-    desc.div.name = 'em'
-
-    # strip all formatting
-    desc.attrs = {}
-    for t in desc.descendants: t.attrs = {}
-
-    desc.insert(0, link)
-    items[i] = desc
-    
+    items = [(tr.div.a, tr.div.div.div) for tr in soup.find_all("tr", itemtype="http://schema.org/Article") if tr.div.a ]
 
 
-clean = bs4.BeautifulSoup("""
-<!doctype html>
-<html>
-<head>
-<meta charset='UTF-8'/>
-<title></title>
-<style>
-body {
-    max-width: 50rem;
-    padding: 2rem;
-    margin: auto;
-    }
-body > div {
-    margin: 1em
-    }
-</style>
-</head>
-<body>
-<h1>neal.news</h1>
-</body>
-</html>
-""")
+    for i, item in enumerate(items):
+        link, desc = item
+        link.attrs = unwrap_link(link['href'])
+        link.span.unwrap()
+        if link.contents[0]  == ' ':
+            del link.contents[0]
+        if link.contents[-1] == ' ':
+            del link.contents[-1]
 
-#clean = bs4.BeautifulSoup(requests.get("http://neal.news").content)
+        #change publisher div to em
+        desc.div.a.span.unwrap()
+        desc.div.a.unwrap()
+        desc.div.name = 'em'
 
-# Inline date
-d = p.get("Date")
-d = datetime.datetime.strptime(d, "%a, %d %b %Y %H:%M:%S %z")
-d = d.strftime('%b %d, %Y')
+        # strip all formatting
+        desc.attrs = {}
+        for t in desc.descendants: t.attrs = {}
 
-clean.head.title.string = "neal.news / " + d
+        desc.insert(0, link)
+        items[i] = desc
 
-h3 = soup.new_tag("h3")
+    return items
+        
 
-h3.string = d
+def build_new_index(items, d):
 
-yesterday = soup.new_tag("a", href = "%s.html" % uuid.uuid1())
-yesterday.string = "yesterday's news"
+    clean = bs4.BeautifulSoup("""
+    <!doctype html>
+    <html>
+    <head>
+    <meta charset='UTF-8'/>
+    <title></title>
+    <style>
+    body {
+        max-width: 50rem;
+        padding: 2rem;
+        margin: auto;
+        }
+    body > div {
+        margin: 1em
+        }
+    </style>
+    </head>
+    <body>
+    <h1>neal.news</h1>
+    </body>
+    </html>
+    """, 'html.parser')
 
-clean.body.h1.insert_after(h3, *items, yesterday)
+    #clean = bs4.BeautifulSoup(requests.get("http://neal.news").content)
 
 
-with open("index.html", 'w') as f: f.write(str(clean))
+    clean.head.title.string = "neal.news / " + d
+
+    h3 = clean.new_tag("h3")
+    h3.string = d
+
+    yesterday = clean.new_tag("a", href = "%s.html" % uuid.uuid1())
+    yesterday.string = "yesterday's news"
+
+    succ = clean.body.h1
+    for i in [h3, *items, yesterday]:
+      succ.insert_after(i)
+      succ = i
+
+    return clean, yesterday['href']
 
 
-# Method 2: Client.put_object()
-client = boto3.client('s3')
-client.copy_object(
-        CopySource={'Bucket': 'www.neal.news', 'Key': 'index.html'},
-        Bucket='www.neal.news',
-        Key=yesterday['href'],
-        ContentType='text/html',
-        ContentEncoding='gzip' )
+#with open("index.html", 'w') as f: f.write(str(clean))
 
-client.put_object(
-        Body=gzip.compress(str(clean).encode('UTF-8')),
-        Bucket='www.neal.news',
-        Key='index.html',
-        ContentType='text/html',
-        ContentEncoding='gzip' )
+
+def update_s3(clean, old):
+    # Method 2: Client.put_object()
+    client = boto3.client('s3')
+    client.copy_object(
+            CopySource={'Bucket': 'www.neal.news',
+                           'Key': 'index.html'},
+            Bucket='www.neal.news',
+            Key=old,
+            ContentType='text/html',
+            ContentEncoding='gzip' )
+
+    client.put_object(
+            Body=gzip.compress(str(clean).encode('UTF-8')),
+            Bucket='www.neal.news',
+            Key='index.html',
+            ContentType='text/html',
+            ContentEncoding='gzip' )
+
+def build_page(f):
+    soup, d = parse_email(f)
+    items = extract_items(soup)
+    return build_new_index(items, d)
+
+
+def lambda_handler(event, context):
+    clean, yesterday = build_page(f)
+    update_s3(clean, yesterday_href)
+
+if __name__ == '__main__' :
+    import sys
+    with open(sys.argv[1]) as f:
+        clean, _ = build_page(f)
+    with open('index.html', 'w') as f:
+        f.write(str(clean))
