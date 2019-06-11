ca5cd45e9ccfacdd4bcaad6a15981b034ad12575
Neal Fultz <nfultz@gmail.com>
2019-06-11 04:01:04 +0000

    now pulling html from s3

--- analysis.py
+++ analysis.py
@@ -3,9 +3,12 @@
 import boto3
 import collections
 import datetime
+import io
+import gzip
 import time
 
 MSG_PREFIX = '(xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx) Method request body before transformations: '
+BUCKET = 'www.neal.news'
 
 def get_logs():
     client = boto3.client('logs')
@@ -37,15 +40,40 @@ def get_logs():
         first_ts = min(first_ts, x['@timestamp'])
         clicks.add(x['@message'][len(MSG_PREFIX):])
     
-    first_ts = datetime.datetime.strptime(first_ts, "%Y-%m-%d %H:%M:%S.%f")
+    first_ts = datetime.datetime.strptime(first_ts, "%Y-%m-%d %H:%M:%S.%f").replace(tzinfo=datetime.timezone.utc)
     return clicks, first_ts
 
 
+def get_docs_keys(client, oldest):
+
+    listing = client.list_objects_v2(Bucket=BUCKET)
+
+    for i in listing['Contents']:
+        if i['LastModified'] > oldest:
+            yield i['Key']
+
+
+def fetch_s3(client, id):
+    print("fetch_s3")
+    obj = client.get_object(Bucket=BUCKET, Key=id)
+    f = io.StringIO(gzip.open(obj['Body']).read().decode('utf-8'))
+    return f
 
 def main(args):
-    clicks, first_ts =  get_logs()
-    print(clicks)
-    print(first_ts)
+    Y, first_ts =  get_logs()
+#    print(Y)
+#    print(first_ts)
+    s3_client = boto3.client('s3')
+    X = get_docs_keys(s3_client, first_ts + datetime.timedelta(-1))
+
+    for k in X:
+        print(k)
+        f = fetch_s3(s3_client, k)
+        lines = f.readlines()
+        lines = map(str.strip, lines)
+        lines = [ line for line in lines if line.startswith("<div>") ]
+        print(lines, "\n\n\n\n\n")
+
 
 if __name__ == "__main__":
     import sys
